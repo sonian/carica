734da6a981b6be9e213b5f1ceda338536d9a22ce
David Leatherman <david.leatherman@sonian.net>
2012-11-28 15:57:01 +0000

    Bump version to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sonian/carica "1.0.1-SNAPSHOT"
+(defproject sonian/carica "1.0.1"
   :description "A flexible configuration library"
   :url "https://github.com/sonian/carica"
   :dependencies [[cheshire "4.0.4"]
