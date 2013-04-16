22d2af2364d689e9d18b88a60160c7b56e0879a1
Joe Gallo <joegallo@gmail.com>
2013-04-16 11:42:14 -0400

    version bumps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject sonian/carica "1.0.3-SNAPSHOT"
   :description "A flexible configuration library"
   :url "https://github.com/sonian/carica"
-  :dependencies [[cheshire "5.0.1"]
+  :dependencies [[cheshire "5.1.1"]
                  [org.clojure/tools.logging "0.2.4"]
                  [org.clojure/tools.reader "0.7.3"]]
   :profiles {:dev
-             {:resource-paths ["etc"],
-              :dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-beta1"]]}}
-  :aliases {"all" ["with-profile" "dev:1.5,dev"]}
+             {:resource-paths ["etc"]
+              :dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :aliases {"all" ["with-profile" "dev:1.4,dev"]}
   ;; For Lein 1
   :dev-dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-resources-path "etc")
